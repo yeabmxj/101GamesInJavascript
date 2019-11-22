6e139a5225dd61d57b4849a0c6723e07bf4fc49c
yeabmxj <yeabsira.r.moges@gmail.com>
2019-11-22 18:14:08 -0500

    tictactoe/ implemented console log x clicks

--- 01.tictactoe/js/main.js
+++ 01.tictactoe/js/main.js
@@ -1,1 +1,41 @@
-console.log('JS is linked')
+//constants
+
+//appstate variables
+
+let board; 		//game board
+let turn = 'X';
+
+//cached element references
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+//event listners
+
+document.getElementById('board').addEventListener('click', handleTurn);
+
+//functions
+function init() {
+	board = [			//positions on the board
+		'', '', '',
+		'', '', '',
+		'', '', '',
+	];
+	render();
+};
+
+function render() {		//iterate over board, add mark where necessary
+	board.forEach(function(mark,index) {
+		squares[index].textContent = mark;
+	});
+};
+
+function handleTurn(event) {
+	let idx = squares.findIndex(function(square) {
+		return square === event.target;
+	});
+
+	board[idx] = turn;
+	console.log(board);
+};
+
+init();
