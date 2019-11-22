ccd85f99d6f00fb7e176c5f7fc53bc087f29cc0d
yeabmxj <yeabsira.r.moges@gmail.com>
2019-11-22 18:45:22 -0500

    tictactoe/ implemented rudimentary win system

--- 01.tictactoe/js/main.js
+++ 01.tictactoe/js/main.js
@@ -1,5 +1,15 @@
 //constants
 
+const winningCombos = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[2,4,6],
+];
+
 //appstate variables
 
 let board; 		//game board
@@ -16,6 +26,29 @@ const messages = document.querySelector('h2');
 document.getElementById('board').addEventListener('click', handleTurn);
 
 //functions
+
+function getWinner() {
+	let winner = null;
+	winningCombos.forEach(function(combo,index){
+	if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {			
+		winner = board[combo[0]];
+	}
+	});
+	return winner ? winner : board.includes('') ? null : 'T';
+};
+
+function handleTurn(event) {
+	let idx = squares.findIndex(function(square) {
+		return square === event.target;
+	});
+
+	board[idx] = turn;
+
+	turn = turn === 'X' ? 'O' : 'X';
+	win = getWinner();
+	render();
+};
+
 function init() {
 	board = [			//positions on the board
 		'', '', '',
@@ -29,19 +62,7 @@ function render() {		//iterate over board, add mark where necessary
 	board.forEach(function(mark,index) {
 		squares[index].textContent = mark;
 	});
-	messages.textContent = `It's ${turn}'s turn!`;
-};
-
-function handleTurn(event) {
-	let idx = squares.findIndex(function(square) {
-		return square === event.target;
-	});
-
-	board[idx] = turn;
-
-	turn = turn === 'X' ? 'O' : 'X';
-
-	render();
+	messages.textContent = win ? `${win} wins the game` : `It's ${turn}'s turn!`;	
 };
 
 init();
