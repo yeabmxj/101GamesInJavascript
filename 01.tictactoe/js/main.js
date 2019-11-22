573f8c8bd3f4c97998be4258911a85199d493ef6
yeabmxj <yeabsira.r.moges@gmail.com>
2019-11-22 18:36:34 -0500

    tictactoe/ register o on board

--- 01.tictactoe/js/main.js
+++ 01.tictactoe/js/main.js
@@ -3,11 +3,13 @@
 //appstate variables
 
 let board; 		//game board
-let turn = 'X';
+let turn = 'X'; //starting move
+let win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
 
 //cached element references
 
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
 
 //event listners
 
@@ -27,6 +29,7 @@ function render() {		//iterate over board, add mark where necessary
 	board.forEach(function(mark,index) {
 		squares[index].textContent = mark;
 	});
+	messages.textContent = `It's ${turn}'s turn!`;
 };
 
 function handleTurn(event) {
@@ -35,7 +38,8 @@ function handleTurn(event) {
 	});
 
 	board[idx] = turn;
-	console.log(board);
+
+	turn = turn === 'X' ? 'O' : 'X';
 
 	render();
 };
